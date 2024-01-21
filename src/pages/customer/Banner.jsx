068089d520e3c09f14e6d963b9a148085b74d9d2
sofeae <sofeaezzaty@gmail.com>
2024-01-21 17:16:16 +0800

    edit banner

--- src/pages/customer/Banner.jsx
+++ src/pages/customer/Banner.jsx
@@ -7,11 +7,10 @@ export const Banner = () => {
         <div className="banner w-full md:w-2/3 px-7 mx-auto relative flex flex-col md:flex-row items-center justify-between">
             <div className="banner-description w-full md:w-full p-3 mb-4 md:mb-0">
                 <h2 className="mb-6 text-3xl font-bold text-black text-center">
-                    Start Order Now!
+                    Start Order Now
                 </h2>
                 <p className="font-semibold text-lg text-yellow-600 py-2 text-center">
-                    Click on the '+' button to add a product to your cart and get your queue number
-                    after completing your payment.
+                    Choose your favourites and add them to cart
                 </p>
                 {/* <div className="btn-container"> */}
                     {/* <Button>Order Now</Button> */}
