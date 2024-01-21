43aedebba8e754d8ee0290a245eb6a5191948d06
sofeae <sofeaezzaty@gmail.com>
2024-01-21 17:45:21 +0800

    try

--- src/utils/imageUrl.js
+++ src/utils/imageUrl.js
@@ -1,5 +1,5 @@
 export const frontendBaseURL =
   "https://bazaarease-frontend-server.onrender.com";
-// export const backendBaseURL = "https://bazaarease-backend.onrender.com";
-export const backendBaseURL = "http://localhost:4000";
-export const imageURL = "https://pub-941a8242b5424f5d8b2600b52797e1f6.r2.dev";
+export const backendBaseURL = "https://bazaarease-backend.onrender.com";
+// export const backendBaseURL = "http://localhost:4000";
+export const imageURL = "https://pub-941a8242b5424f5d8b2600b52797e1f6.r2.dev"; 
