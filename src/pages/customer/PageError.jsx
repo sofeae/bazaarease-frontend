875fdd31ce3fd179b47a5a9d15261f738c4a5957
sofeae <sofeaezzaty@gmail.com>
2024-01-22 09:25:56 +0800

    page error

--- src/pages/customer/PageError.jsx
+++ src/pages/customer/PageError.jsx
@@ -6,7 +6,8 @@ const PageError = () => {
             <div className="banner w-full md:w-2/3 px-7 mx-auto relative flex flex-col md:flex-row items-center justify-between">
                 <div className="banner-description w-full md:w-full p-3 mb-4 md:mb-0">
                     <p className="font-semibold text-lg text-yellow-600 py-2 text-center">
-                        Sorry, the stall is closed right now :(
+                        Sorry, the stall is closed right now :( <br></br>
+                        Please scan the QR code again later to order.
                     </p>
                 </div>
             </div>
